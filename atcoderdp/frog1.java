f989292593cd457411adfe23903b2f6c4dfe430b
Sai Kaushik Kannan <saikaushik333@gmai.com>
2020-05-23 20:16:32 +0530

    atcoder dp

---
+++ atcoderdp/frog1.java
@@ -0,0 +1,203 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    static class InputReader {
+
+        private final InputStream stream;
+        private final byte[] buf = new byte[8192];
+        private int curChar, snumChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int snext() {
+            if (snumChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= snumChars) {
+                curChar = 0;
+                try {
+                    snumChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (snumChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        public String readString() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public String nextLine() {
+            int c = snext();
+            while (isSpaceChar(c))
+                c = snext();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+
+    }
+    static class OutputWriter {
+		private final PrintWriter writer;
+ 
+		public OutputWriter(OutputStream outputStream) {
+			writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+		}
+ 
+		public OutputWriter(Writer writer) {
+			this.writer = new PrintWriter(writer);
+		}
+ 
+		public void print(Object...objects) {
+			for (int i = 0; i < objects.length; i++) {
+				if (i != 0)
+					writer.print(' ');
+				writer.print(objects[i]);
+			}
+		}
+ 
+		public void printLine(Object...objects) {
+			print(objects);
+			writer.println();
+		}
+ 
+		public void close() {
+			writer.close();
+		}
+ 
+		public void flush() {
+			writer.flush();
+		}
+ 
+		}
+ 
+    static class IOUtils {
+ 
+		public static int[] readIntArray(InputReader in, int size) {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+                array[i] = in.nextInt();
+            return array;
+        }
+
+    }
+    static int gcd(int a, int b) 
+    { 
+        if (a == 0) 
+            return b; 
+        return gcd(b % a, a); 
+    } 
+    static int mod = (int)(1e9+7);
+    public static long pow(long a,long b)
+    {
+        long ans = 1;
+        while(b> 0)
+        {
+            if((b & 1)==1){
+                ans = (ans*a) % mod; 
+            }
+            a = (a*a) % mod;
+            b = b>>1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+        //IOUtils io = new IOUtils();
+        int n = in.nextInt();
+        int[] arr = in.nextIntArray(n);
+        int[] dp = new int[n];
+        dp[0] = 0;
+        dp[1] = Math.abs(arr[0]-arr[1]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i] = Math.min(Math.abs(arr[i]-arr[i-1])+dp[i-1],Math.abs(arr[i]-arr[i-2])+dp[i-2]);
+        }
+        out.printLine(dp[n-1]);
+        out.flush();
+        out.close();
+    }
+}
---
+++ atcoderdp/knapsack2.java
@@ -0,0 +1,235 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    static class InputReader {
+
+        private final InputStream stream;
+        private final byte[] buf = new byte[8192];
+        private int curChar, snumChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int snext() {
+            if (snumChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= snumChars) {
+                curChar = 0;
+                try {
+                    snumChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (snumChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        public String readString() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public String nextLine() {
+            int c = snext();
+            while (isSpaceChar(c))
+                c = snext();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+
+    }
+    static class OutputWriter {
+		private final PrintWriter writer;
+ 
+		public OutputWriter(OutputStream outputStream) {
+			writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+		}
+ 
+		public OutputWriter(Writer writer) {
+			this.writer = new PrintWriter(writer);
+		}
+ 
+		public void print(Object...objects) {
+			for (int i = 0; i < objects.length; i++) {
+				if (i != 0)
+					writer.print(' ');
+				writer.print(objects[i]);
+			}
+		}
+ 
+		public void printLine(Object...objects) {
+			print(objects);
+			writer.println();
+		}
+ 
+		public void close() {
+			writer.close();
+		}
+ 
+		public void flush() {
+			writer.flush();
+		}
+ 
+		}
+ 
+    static class IOUtils {
+ 
+		public static int[] readIntArray(InputReader in, int size) {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+                array[i] = in.nextInt();
+            return array;
+        }
+
+    }
+    static int gcd(int a, int b) 
+    { 
+        if (a == 0) 
+            return b; 
+        return gcd(b % a, a); 
+    } 
+    static int mod = (int)(1e9+7);
+    public static long pow(long a,long b)
+    {
+        long ans = 1;
+        while(b> 0)
+        {
+            if((b & 1)==1){
+                ans = (ans*a) % mod; 
+            }
+            a = (a*a) % mod;
+            b = b>>1;
+        }
+        return ans;
+    }
+    static long INF = (long)(1e18+5);
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+        //IOUtils io = new IOUtils();
+        int n = in.nextInt();
+        int wt = in.nextInt();
+        int[] v = new int[n];
+        int[] w = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            w[i] = in.nextInt();
+            v[i] = in.nextInt();
+        }
+        int sum = 0;
+        for(int x:v)
+        {
+            sum+=x;
+        }
+        long[] dp = new long[sum+1];
+        Arrays.fill(dp,INF);
+        dp[0] = 0;
+        for(int i=1;i<=n;i++)
+        {
+            long[] temp = new long[sum+1];
+            for(int j=1;j<=sum;j++)
+            {
+                if(i==0 || j==0)
+                    temp[j] = 0;        
+                else if(j>=v[i-1])
+                {
+                    temp[j] = Math.min(w[i-1]+dp[j-v[i-1]],dp[j]);
+                }
+                else
+                    temp[j] = dp[j];
+            }
+            dp = temp;
+        }
+        long answer = 0;
+        for(int i=0;i<=sum;i++)
+        {
+            if(dp[i]<=wt)
+            {
+                answer = Math.max(answer,i);
+            }
+        }
+        out.printLine(answer);
+        out.flush();
+        out.close();
+    }
+}
