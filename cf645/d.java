55ef14451b7b40ad5f8b8aa853ecf9dde42d31c6
Sai Kaushik Kannan <saikaushik333@gmai.com>
2020-05-27 22:26:05 +0530

    may cookoff and cf645

---
+++ cf645/d.java
@@ -0,0 +1,239 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    static class InputReader {
+
+        private final InputStream stream;
+        private final byte[] buf = new byte[8192];
+        private int curChar, snumChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int snext() {
+            if (snumChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= snumChars) {
+                curChar = 0;
+                try {
+                    snumChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (snumChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        public String readString() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public String nextLine() {
+            int c = snext();
+            while (isSpaceChar(c))
+                c = snext();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+
+    }
+    static class OutputWriter {
+		private final PrintWriter writer;
+ 
+		public OutputWriter(OutputStream outputStream) {
+			writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+		}
+ 
+		public OutputWriter(Writer writer) {
+			this.writer = new PrintWriter(writer);
+		}
+ 
+		public void print(Object...objects) {
+			for (int i = 0; i < objects.length; i++) {
+				if (i != 0)
+					writer.print(' ');
+				writer.print(objects[i]);
+			}
+		}
+ 
+		public void printLine(Object...objects) {
+			print(objects);
+			writer.println();
+		}
+ 
+		public void close() {
+			writer.close();
+		}
+ 
+		public void flush() {
+			writer.flush();
+		}
+ 
+		}
+ 
+    static class IOUtils {
+ 
+		public static int[] readIntArray(InputReader in, int size) {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+                array[i] = in.nextInt();
+            return array;
+        }
+
+    }
+    static int gcd(int a, int b) 
+    { 
+        if (a == 0) 
+            return b; 
+        return gcd(b % a, a); 
+    } 
+    static int mod = (int)(1e9+7);
+    public static long pow(long a,long b)
+    {
+        long ans = 1;
+        while(b> 0)
+        {
+            if((b & 1)==1){
+                ans = (ans*a) % mod; 
+            }
+            a = (a*a) % mod;
+            b = b>>1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        OutputWriter out = new OutputWriter(System.out);
+        int n = in.nextInt();
+        long x = in.nextLong();
+        long[] arr = new long[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = in.nextLong();
+        }
+        long curSum = 0, curD = 0,res = 0;
+        int l = n-1,r = n-1;
+        while(r>=0)
+        {
+            out.printLine(l + " " + r);
+            while((curD+arr[l])<=x)
+            {
+                curD+=arr[l];
+                long temp = arr[l]*(arr[l]+1);
+                temp=temp>>1;
+                curSum+=temp;
+                l--;
+                if(l<0)
+                    l = n-1;
+            }
+            long left = x-curD;
+            long temp = ((arr[l])*(arr[l]+1))-((arr[l]-left)*(arr[l]-left+1));
+           // long temp = left*(2*arr[l]-left+1);
+          //  out.printLine(temp1 + " " + temp);
+            temp = temp>>1;
+            curSum+=temp;
+            res = Math.max(res,curSum);
+            if(arr[r]<=x)
+            {
+                curD-=arr[r];
+            }
+            else{
+                curD = 0;
+                l--;
+            }
+            long sub = arr[r]*(arr[r]+1);
+            sub = sub>>1;
+            if(arr[r]<=x)
+            {
+                curSum-=sub;
+            }
+            curSum-=temp;
+            r--;
+        }
+        out.printLine(res);
+        out.flush();
+        out.close();
+    }
+}
---
+++ codechefmaycookoff/b.java
@@ -0,0 +1,270 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    static class InputReader {
+
+        private final InputStream stream;
+        private final byte[] buf = new byte[8192];
+        private int curChar, snumChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int snext() {
+            if (snumChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= snumChars) {
+                curChar = 0;
+                try {
+                    snumChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (snumChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public long nextLong() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = snext();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res *= 10;
+                res += c - '0';
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public int[] nextIntArray(int n) {
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        public String readString() {
+            int c = snext();
+            while (isSpaceChar(c)) {
+                c = snext();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public String nextLine() {
+            int c = snext();
+            while (isSpaceChar(c))
+                c = snext();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = snext();
+            } while (!isEndOfLine(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null)
+                return filter.isSpaceChar(c);
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+
+    }
+    static class OutputWriter {
+		private final PrintWriter writer;
+ 
+		public OutputWriter(OutputStream outputStream) {
+			writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+		}
+ 
+		public OutputWriter(Writer writer) {
+			this.writer = new PrintWriter(writer);
+		}
+ 
+		public void print(Object...objects) {
+			for (int i = 0; i < objects.length; i++) {
+				if (i != 0)
+					writer.print(' ');
+				writer.print(objects[i]);
+			}
+		}
+ 
+		public void printLine(Object...objects) {
+			print(objects);
+			writer.println();
+		}
+ 
+		public void close() {
+			writer.close();
+		}
+ 
+		public void flush() {
+			writer.flush();
+		}
+ 
+		}
+ 
+    static class IOUtils {
+ 
+		public static int[] readIntArray(InputReader in, int size) {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+                array[i] = in.nextInt();
+            return array;
+        }
+
+    }
+    static int gcd(int a, int b) 
+    { 
+        if (a == 0) 
+            return b; 
+        return gcd(b % a, a); 
+    } 
+    static int mod = (int)(1e9+7);
+    public static long pow(long a,long b)
+    {
+        long ans = 1;
+        while(b> 0)
+        {
+            if((b & 1)==1){
+                ans = (ans*a) % mod; 
+            }
+            a = (a*a) % mod;
+            b = b>>1;
+        }
+        return ans;
+    }
+    // static int[] map;
+    // static int oddindex,oddCount;
+    public static boolean canPermutePalindrome(int[] arr,int oddCount,int oddindex,int[] map) {
+        for (int i = 0; i < arr.length; i++) {
+            map[arr[i]]++;
+        }
+        int count = 0;
+        for (int key = 0; key < map.length && count <= 1; key++) {
+            if(map[key]%2==1){
+                count += 1;
+                oddindex = key;
+            }
+             
+        }
+        oddCount = count;
+        return count <= 1;
+    }
+    public static void main(String[] args) {
+        try{
+            InputReader in = new InputReader(System.in);
+            OutputWriter out = new OutputWriter(System.out);
+            //IOUtils io = new IOUtils();
+            int t = in.nextInt();
+            while(t-- >0)
+            {
+                int n = in.nextInt(),m = in.nextInt();
+                int[][] arr = new int[n][m];
+                for(int i=0;i<n;i++)
+                {
+                    arr[i] = in.nextIntArray(m);
+                }
+                boolean flag = true;
+                StringBuilder res = new StringBuilder();
+                for(int k=0;k<n;k++)
+                {
+                    int oddindex = -1;
+                    int oddCount=0;
+                    int[] map = new int[128];
+                    if(!canPermutePalindrome(arr[k],oddCount,oddindex,map))
+                    {
+                        flag = false;
+                        break;
+                    }
+                    else{
+                        StringBuilder fh = new StringBuilder();
+                        StringBuilder sh = new StringBuilder();
+                        for(int i=0;i<map.length;i++)
+                        {
+                            if(map[i]>0)
+                            {
+                                StringBuilder temp = new StringBuilder();
+                                for(int x=0;x<(map[i]/2);x++)
+                                {
+                                    temp.append((char)(i+'0') + " ");
+                                }
+                                fh.append(temp);
+                                sh.insert(0,temp);
+                            }
+                        }
+                        // out.printLine(sb);
+                        if(oddCount==1)
+                        {
+                            res.append(fh + "" + (char)(oddindex+'0') + " " + sh + "\n");
+                        }
+                        else
+                            res.append(fh + "" + sh + "\n");
+                    }
+                }
+                if(!flag)
+                {
+                    out.printLine("-1");
+                }
+                else
+                {
+                    out.print(res);
+                }
+            }
+            out.flush();
+            out.close();
+        }
+        catch(Exception e){
+            return;
+        }
+    }
+    
+}
